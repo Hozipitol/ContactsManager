e1539d5bdde730217ca925f6061c0db52d014d67
Huzefa <huzefapitol67@gmail.com>
2020-10-12 16:07:11 +0530

    Updates

---
+++ precache-manifest.00fe832b4f507fb50c9e0f34d73632c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae15a7ad39c29bf571b54666d3b51ebd",
+    "url": "/ContactsManager/index.html"
+  },
+  {
+    "revision": "c8e48c5c4869dd0fd867",
+    "url": "/ContactsManager/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9781be9e6ccfe4997912",
+    "url": "/ContactsManager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "c8e48c5c4869dd0fd867",
+    "url": "/ContactsManager/static/js/2.4b36d575.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ContactsManager/static/js/2.4b36d575.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9781be9e6ccfe4997912",
+    "url": "/ContactsManager/static/js/main.88d1bd3e.chunk.js"
+  },
+  {
+    "revision": "37977ad6087c5470180f",
+    "url": "/ContactsManager/static/js/runtime-main.fb8c1e20.js"
+  }
+]);
